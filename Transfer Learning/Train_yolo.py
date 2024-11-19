fa03b98cd11a43f0fb18670316b900898ab67c6c
ELgagadhkt <61547651+ELgagadhkt@users.noreply.github.com>
2024-11-19 22:24:32 +0100

    transfer learning before training v2

--- Transfer Learning/Train_yolo.py
+++ Transfer Learning/Train_yolo.py
@@ -3,8 +3,5 @@ from ultralytics import YOLO
 # Charger le modèle pré-entrainé
 model = YOLO("yolo-Weights/yolov8n.pt")
 
-# Réinitialiser les classes pour correspondre à vos nouvelles classes
-model.reset_head(num_classes=3)  # Vos nouvelles classes uniquement
-
 # Lancer l'entraînement avec les nouvelles données
 model.train(data="data.yaml", epochs=50, imgsz=640, batch=16)
